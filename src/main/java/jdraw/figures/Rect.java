8d2ea6254e2a507e64bf5159ec1afbeee1978ef7
Florian Thiévent <florianjeremias@gmail.com>
2019-09-26 00:08:59 +0200

    implemented Ex2 --> pitfall remove all (see fixme)

--- src/main/java/jdraw/figures/Rect.java
+++ src/main/java/jdraw/figures/Rect.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018 Fachhochschule Nordwestschweiz (FHNW)
- * All Rights Reserved. 
+ * All Rights Reserved.
  */
 
 package jdraw.figures;
@@ -9,94 +9,111 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 import java.util.List;
 
 import jdraw.framework.Figure;
+import jdraw.framework.FigureEvent;
 import jdraw.framework.FigureHandle;
 import jdraw.framework.FigureListener;
 
 /**
  * Represents rectangles in JDraw.
- * 
- * @author Christoph Denzler
  *
+ * @author Christoph Denzler
  */
 public class Rect implements Figure {
-	private static final long serialVersionUID = 9120181044386552132L;
+    private static final long serialVersionUID = 9120181044386552132L;
 
-	/**
-	 * Use the java.awt.Rectangle in order to save/reuse code.
-	 */
-	private final Rectangle rectangle;
-	
-	/**
-	 * Create a new rectangle of the given dimension.
-	 * @param x the x-coordinate of the upper left corner of the rectangle
-	 * @param y the y-coordinate of the upper left corner of the rectangle
-	 * @param w the rectangle's width
-	 * @param h the rectangle's height
-	 */
-	public Rect(int x, int y, int w, int h) {
-		rectangle = new Rectangle(x, y, w, h);
-	}
+    private ArrayList<FigureListener> listeners = new ArrayList<>();
 
-	/**
-	 * Draw the rectangle to the given graphics context.
-	 * @param g the graphics context to use for drawing.
-	 */
-	@Override
-	public void draw(Graphics g) {
-		g.setColor(Color.WHITE);
-		g.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-		g.setColor(Color.BLACK);
-		g.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-	}
-	
-	@Override
-	public void setBounds(Point origin, Point corner) {
-		rectangle.setFrameFromDiagonal(origin, corner);
-		// TODO notification of change
-	}
 
-	@Override
-	public void move(int dx, int dy) {
-		rectangle.setLocation(rectangle.x + dx, rectangle.y + dy);
-		// TODO notification of change
-	}
+    /**
+     * Use the java.awt.Rectangle in order to save/reuse code.
+     */
+    private final Rectangle rectangle;
 
-	@Override
-	public boolean contains(int x, int y) {
-		return rectangle.contains(x, y);
-	}
+    /**
+     * Create a new rectangle of the given dimension.
+     *
+     * @param x the x-coordinate of the upper left corner of the rectangle
+     * @param y the y-coordinate of the upper left corner of the rectangle
+     * @param w the rectangle's width
+     * @param h the rectangle's height
+     */
+    public Rect(int x, int y, int w, int h) {
+        rectangle = new Rectangle(x, y, w, h);
+    }
 
-	@Override
-	public Rectangle getBounds() {
-		return rectangle.getBounds();
-	}
+    /**
+     * Draw the rectangle to the given graphics context.
+     *
+     * @param g the graphics context to use for drawing.
+     */
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        g.setColor(Color.BLACK);
+        g.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    }
 
-	/**
-	 * Returns a list of 8 handles for this Rectangle.
-	 * @return all handles that are attached to the targeted figure.
-	 * @see jdraw.framework.Figure#getHandles()
-	 */	
-	@Override
-	public List<FigureHandle> getHandles() {
-		return null;
-	}
+    @Override
+    public void setBounds(Point origin, Point corner) {
+        rectangle.setFrameFromDiagonal(origin, corner);
+        // TODO notification of change
+        notifyListeners();
+    }
 
-	@Override
-	public void addFigureListener(FigureListener listener) {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void move(int dx, int dy) {
+        rectangle.setLocation(rectangle.x + dx, rectangle.y + dy);
+        // TODO notification of change
+        notifyListeners();
+    }
 
-	@Override
-	public void removeFigureListener(FigureListener listener) {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public boolean contains(int x, int y) {
+        return rectangle.contains(x, y);
+    }
 
-	@Override
-	public Figure clone() {
-		return null;
-	}
+    @Override
+    public Rectangle getBounds() {
+        return rectangle.getBounds();
+    }
+
+    /**
+     * Returns a list of 8 handles for this Rectangle.
+     *
+     * @return all handles that are attached to the targeted figure.
+     * @see jdraw.framework.Figure#getHandles()
+     */
+    @Override
+    public List<FigureHandle> getHandles() {
+        return null;
+    }
+
+    @Override
+    public void addFigureListener(FigureListener listener) {
+        listeners.add(listener);
+
+    }
+
+    @Override
+    public void removeFigureListener(FigureListener listener) {
+        listeners.remove(listener);
+    }
+
+    @Override
+    public Figure clone() {
+        return null;
+    }
+
+    /**
+     * Notify all registered Listeners
+     */
+    private void notifyListeners() {
+        listeners.forEach(e -> e.figureChanged(new FigureEvent(this)));
+    }
 
 }
--- src/main/java/jdraw/std/StdDrawModel.java
+++ src/main/java/jdraw/std/StdDrawModel.java
@@ -1,86 +1,110 @@
 /*
  * Copyright (c) 2018 Fachhochschule Nordwestschweiz (FHNW)
- * All Rights Reserved. 
+ * All Rights Reserved.
  */
 
 package jdraw.std;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
-import jdraw.framework.DrawCommandHandler;
-import jdraw.framework.DrawModel;
-import jdraw.framework.DrawModelListener;
-import jdraw.framework.Figure;
+import jdraw.framework.*;
 
 /**
  * Provide a standard behavior for the drawing model. This class initially does not implement the methods
  * in a proper way.
  * It is part of the course assignments to do so.
- * @author TODO add your name here
  *
+ * @author TODO add your name here
  */
 public class StdDrawModel implements DrawModel {
 
-	private List<Figure> figures = new ArrayList<>();
+    private LinkedList<Figure> figures = new LinkedList<>();
+    private List<DrawModelListener> listeners = new ArrayList<>();
+    private FigureListener figureListener = e -> notifyListener(e.getFigure(), DrawModelEvent.Type.DRAWING_CHANGED);
 
-	@Override
-	public void addFigure(Figure f) {
-		// TODO to be implemented
-		// System.out.println("StdDrawModel.addFigure has to be implemented");
-		figures.add(f);
-	}
 
-	@Override
-	public Stream<Figure> getFigures() {
-		// TODO to be implemented  
-		// System.out.println("StdDrawModel.getFigures has to be implemented");
-		return figures.stream(); // Only guarantees, that the application starts -- has to be replaced !!!
-	}
+    /**
+     * Notify all Listeners about a change
+     *
+     * @param f figure
+     * @param type eventtype
+     */
+    private void notifyListener(Figure f, DrawModelEvent.Type type) {
+        listeners.forEach(e -> e.modelChanged(new DrawModelEvent(this, f, type)));
+    }
 
-	@Override
-	public void removeFigure(Figure f) {
-		// TODO to be implemented  
-		// System.out.println("StdDrawModel.removeFigure has to be implemented");
-		figures.remove(f);
-	}
 
-	@Override
-	public void addModelChangeListener(DrawModelListener listener) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.addModelChangeListener has to be implemented");
-	}
+    @Override
+    public void addFigure(Figure f) {
+        if (figures.contains(f)) return;
+        figures.add(f);
+        f.addFigureListener(figureListener);
+        notifyListener(f, DrawModelEvent.Type.FIGURE_ADDED);
+    }
 
-	@Override
-	public void removeModelChangeListener(DrawModelListener listener) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.removeModelChangeListener has to be implemented");
-	}
+    @Override
+    public Stream<Figure> getFigures() {
+        return figures.stream();
+    }
 
-	/** The draw command handler. Initialized here with a dummy implementation. */
-	// TODO initialize with your implementation of the undo/redo-assignment.
-	private DrawCommandHandler handler = new EmptyDrawCommandHandler();
+    @Override
+    public void removeFigure(Figure f) {
+        if(!figures.contains(f)) {
+            throw new IllegalArgumentException("This figure does not exists in the list");
+        }
+        figures.remove(f);
+        notifyListener(f, DrawModelEvent.Type.FIGURE_REMOVED);
+    }
 
-	/**
-	 * Retrieve the draw command handler in use.
-	 * @return the draw command handler.
-	 */
-	@Override
-	public DrawCommandHandler getDrawCommandHandler() {
-		return handler;
-	}
+    @Override
+    public void addModelChangeListener(DrawModelListener listener) {
+        // TODO to be implemented
+        //System.out.println("StdDrawModel.addModelChangeListener has to be implemented");
+        listeners.add(listener);
+    }
 
-	@Override
-	public void setFigureIndex(Figure f, int index) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.setFigureIndex has to be implemented");
-	}
+    @Override
+    public void removeModelChangeListener(DrawModelListener listener) {
+        if(!listeners.contains(listener)){
+            throw new IllegalArgumentException("This listener does not exist");
+        }
+        listeners.remove(listener);
+    }
 
-	@Override
-	public void removeAllFigures() {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.removeAllFigures has to be implemented");
-	}
+    /**
+     * The draw command handler. Initialized here with a dummy implementation.
+     */
+    // TODO initialize with your implementation of the undo/redo-assignment.
+    private DrawCommandHandler handler = new EmptyDrawCommandHandler();
+
+    /**
+     * Retrieve the draw command handler in use.
+     *
+     * @return the draw command handler.
+     */
+    @Override
+    public DrawCommandHandler getDrawCommandHandler() {
+        return handler;
+    }
+
+
+    @Override
+    public void setFigureIndex(Figure f, int index) {
+        if(!figures.contains(f)){
+            throw new IllegalArgumentException("This figure does not exist");
+        }
+        figures.remove(f);
+        figures.add(index, f);
+        notifyListener(f, DrawModelEvent.Type.FIGURE_CHANGED);
+    }
+
+    //FIXME
+    @Override
+    public void removeAllFigures() {
+        figures.forEach(f -> removeFigure(f));
+    }
 
 }
