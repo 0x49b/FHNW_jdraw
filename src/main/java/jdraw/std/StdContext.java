f65271cdf88e97085322e4359bfbed7cd28188c3
Florian Thiévent <florian.thievent@sbb.ch>
2019-10-02 15:10:53 +0200

    added new draw tool oval

--- src/main/java/jdraw/std/StdContext.java
+++ src/main/java/jdraw/std/StdContext.java
@@ -16,6 +16,7 @@ import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import jdraw.figures.OvalTool;
 import jdraw.figures.RectTool;
 import jdraw.framework.DrawCommandHandler;
 import jdraw.framework.DrawModel;
@@ -166,7 +167,10 @@ public class StdContext extends AbstractContext {
 	protected void doRegisterDrawTools() {
 		// TODO Add new figure tools here
 		DrawTool rectangleTool = new RectTool(this);
+		DrawTool ovalTool = new OvalTool(this);
 		addTool(rectangleTool);
+		addTool(ovalTool);
+
 	}
 
 	/**
